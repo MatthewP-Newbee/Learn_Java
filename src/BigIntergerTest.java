dd592779bb8e26bbd1ba5800670aab3adb5cb4db
MatthewP <matthewhakka@gmail.com>
2018-11-16 18:07:12 +0800

    update

--- src/BigIntergerTest.java
+++ src/BigIntergerTest.java
@@ -13,12 +13,12 @@ public class BigIntergerTest
         Scanner in = new Scanner(System.in);
 
         System.out.print("How many numbers do you need to draw?");
-        int n = in.nextInt();
+        int k = in.nextInt();
 
         BigInteger lottoryOdds = BigInteger.valueOf(1);
 
         for (int i = 1; i <= k; i++)
-            lottoryOdds = lottoryOdds.multipl(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i));
+            lottoryOdds = lottoryOdds.multiply(BigInteger.valueOf(k - i + 1)).divide(BigInteger.valueOf(i));
 
         System.out.println("Your odds are 1 in " + lottoryOdds + ". Good Luck!");
     }
